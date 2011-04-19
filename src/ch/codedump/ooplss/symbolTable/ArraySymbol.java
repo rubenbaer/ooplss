8b75ad9a13c7a67bdf92ba26eefda45ac1878206
Stefan Heinemann <heins4@bfh.ch>
2011-04-19 15:59:53 +0200

    Grammar, Src: Introduced arrays

---
+++ src/ch/codedump/ooplss/symbolTable/ArraySymbol.java
@@ -0,0 +1,7 @@
+package ch.codedump.ooplss.symbolTable;
+
+public class ArraySymbol extends Symbol {
+	public ArraySymbol(String name, Scope scope) {
+		super(name, scope);
+	}
+}
---
+++ src/ch/codedump/ooplss/symbolTable/exceptions/NotAnArrayException.java
@@ -0,0 +1,23 @@
+package ch.codedump.ooplss.symbolTable.exceptions;
+
+import org.antlr.runtime.Token;
+
+import ch.codedump.ooplss.tree.OoplssAST;
+
+public class NotAnArrayException extends Exception {
+	private static final long serialVersionUID = -737683182073140803L;
+	
+	protected OoplssAST node;
+
+	public NotAnArrayException(OoplssAST node) {
+		this.node = node;
+	}
+	
+	public String toString() {
+		Token t = this.node.token;
+		
+		return "line " + t.getLine() + ":" + t.getTokenIndex() + " symbol " + 
+		this.node.getText() + 
+		" is not an arra";
+	}
+}
