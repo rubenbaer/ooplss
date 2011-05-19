01d9bdb6aecc8aa0aa9e1541e956ce8404208636
Stefan Heinemann <heins4@bfh.ch>
2011-05-19 10:34:13 +0200

    Src: There were two test cases for return testing

--- test/ch/codedump/ooplss/test/TestReturn.java
+++ test/ch/codedump/ooplss/test/TestReturn.java
@@ -63,4 +63,90 @@ public class TestReturn extends OoplssTest {
 		this.createTyper(str);
 		ErrorHandler.getInstance().throwException();
 	}
+	
+	@Test
+	public void testReturnNewObject() throws Exception {
+		String str	=	"class foo {\n" + 
+						"	def bar():foo {\n" + 
+						"		return new foo();\n" + 
+						"	}\n" + 
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
+	
+	@Test
+	public void testReturn1() throws Exception {
+		String str = 	"class A {" +
+						"	var x:Int;" +
+						"	def blah():Int {" +
+						"		return x;" +
+						"	}" +
+						"	def blubb():Void {" +
+						"		blah();" +
+						"	}" +
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
+	
+	@Test
+	public void testReturnVoid() throws Exception {
+		String str = 	"class A {" +
+						"	def blah():Void {" +
+						"		return;" +
+						"	}" +
+						"	def blubb():Void {" +
+						"		blah();" +
+						"	}" +
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
+	
+	@Test (expected=WrongReturnValueException.class)
+	public void testInvalidReturnVoid() throws Exception {
+		String str = 	"class A {" +
+						"	def blah():Int {" +
+						"		return;" +
+						"	}" +
+						"	def blubb():Void {" +
+						"		blah();" +
+						"	}" +
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
+	
+	@Test
+	public void testNestedReturnVoid() throws Exception {
+		String str = 	"class A {" +
+						"	var x:Int;" +
+						"	def blah():Int {" +
+						"		if (true) {" +
+						"			return x;" +
+						"		}" +
+						"	}" +
+						"	def blubb():Void {" +
+						"		blah();" +
+						"	}" +
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
+	
+	@Test (expected=WrongReturnValueException.class)
+	public void testInvalidReturn() throws Exception {
+		String str = 	"class A {" +
+						"	var x:String;" +
+						"	def blah():Int {" +
+						"		return x;" +
+						"	}" +
+						"	def blubb():Void {" +
+						"		blah();" +
+						"	}" +
+						"}";
+		this.createTyper(str);
+		ErrorHandler.getInstance().throwException();
+	}
 }
