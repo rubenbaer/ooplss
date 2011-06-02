315eb5a755ab660d9eb3f68bd40284351625dfb5
Stefan Heinemann <heins4@bfh.ch>
2011-06-02 16:54:32 +0200

    Src: Corrected the token position for argument exceptions

--- src/ch/codedump/ooplss/symbolTable/exceptions/ArgumentDoesntMatchException.java
+++ src/ch/codedump/ooplss/symbolTable/exceptions/ArgumentDoesntMatchException.java
@@ -9,7 +9,7 @@ public class ArgumentDoesntMatchException extends OoplssException {
 	private static final long serialVersionUID = 4977646527530717530L;
 
 	public ArgumentDoesntMatchException(OoplssAST node, int argCount) {
-		super(node.token);
+		super(((OoplssAST)node.getChild(0)).token);
 		
 		String err = "Argument " + argCount + " does not match "
 			+ "the method";
